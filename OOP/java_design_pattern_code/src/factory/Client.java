f9ef49ffddaeb307d27e6e1bafa66b7048f12c46
zhangyang27 <78567665@qq.com>
2017-02-23 23:09:00 +0800

    工厂方法模式

--- OOP/java_design_pattern_code/src/factory/Client.java
+++ OOP/java_design_pattern_code/src/factory/Client.java
@@ -1,6 +1,13 @@
 package factory;
 
-
+/**
+ * 设计模式的艺术软件开发人员内功修炼之道_刘伟 P73
+ * 使用工厂方法模式设计一个程序来读取各种不同类型的图片格式，针对每一种图片格式都设计一个图片读取器
+ * 例如：GIF图片读取器读取GIF格式的图片，JPG图片读取器读取JPG格式的图片。
+ * 需要充分考虑系统的灵活性和扩展性。
+ * @author zhangyang27
+ *
+ */
 public class Client {
 
 	public static void main(String[] args) {
