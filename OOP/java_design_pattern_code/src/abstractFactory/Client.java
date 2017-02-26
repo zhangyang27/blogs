a29e9b215df466a77a09bdf1473745521285e7d8
zhangyang27 <78567665@qq.com>
2017-02-26 18:06:01 +0800

    抽象工厂

--- OOP/java_design_pattern_code/src/abstractFactory/Client.java
+++ OOP/java_design_pattern_code/src/abstractFactory/Client.java
@@ -1,6 +1,16 @@
 package abstractFactory;
 
-
+/**
+ * 设计模式的艺术软件开发人员内功修炼之道_刘伟 P86
+ * 对于手机游戏软件
+ * 该软件需要支持Symbian/Android/Windows等多个平台
+ * 针对不同的平台，
+ * 需要实现不同的游戏控制操作类：OperationController 和 界面控制操作类InterfaceController
+ * 请使用工厂类来封装这类的初始化过程。
+ * 为了支持新的操作系统，请使用工厂模式对其进行设计。
+ * @author zhangyang27
+ *
+ */
 public class Client {
 
 	public static void main(String[] args) {
